5e139e649818f7cce31a1288e7652b4e467a564c
jiangdingyi <jiangdingyi@bignox.com>
2019-07-05 13:49:31 +0800

    图片识别

---
+++ server/app/public/dist/precache-manifest.9b477c8adc6b9e260a78b9d22e24863e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "10bad2c77318c7d4317fa26b3850f422",
+    "url": "/static/media/logo.10bad2c7.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "42cb0a3e748fcbb5962a",
+    "url": "/static/js/main.42cb0a3e.chunk.js"
+  },
+  {
+    "revision": "66abc95e93fbca38d5b5",
+    "url": "/static/js/2.66abc95e.chunk.js"
+  },
+  {
+    "revision": "42cb0a3e748fcbb5962a",
+    "url": "/static/css/main.8abf034c.chunk.css"
+  },
+  {
+    "revision": "66abc95e93fbca38d5b5",
+    "url": "/static/css/2.12a79b34.chunk.css"
+  },
+  {
+    "revision": "efa6c64bb8401b412f96a6e88b12459f",
+    "url": "/index.html"
+  }
+];
